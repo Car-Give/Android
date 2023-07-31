a75d320089e73b7f84d1d256b62472ac7ea40eab
posite <su84053@gmail.com>
2023-07-31 20:53:26 +0900

    chore: 디렉토리 구조 변경 및 초기 화면 변경

--- app/src/main/java/com/example/cargive/feat/login/LoginActivity.kt
+++ app/src/main/java/com/example/cargive/feat/login/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.example.cargive
+package com.example.cargive.feat.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cargive.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
