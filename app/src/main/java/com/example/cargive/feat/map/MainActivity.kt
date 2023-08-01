b57f83cf729a1519f1447dabc87bbcbb853cdd30
posite <su84053@gmail.com>
2023-08-01 17:50:01 +0900

    feat: 현재 위치로 이동하기 기능 구현

--- app/src/main/java/com/example/cargive/feat/map/MainActivity.kt
+++ app/src/main/java/com/example/cargive/feat/map/MainActivity.kt
@@ -2,11 +2,62 @@ package com.example.cargive.feat.map
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 import com.example.cargive.R
+import com.example.cargive.databinding.ActivityMainBinding
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.util.FusedLocationSource
+import java.security.Permissions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mapFragment: MapFragment
+    private lateinit var locationSource: FusedLocationSource
+    private lateinit var naverMap: NaverMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+        val fm = supportFragmentManager
+
+        mapFragment = fm.findFragmentById(binding.mapFragment.id) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(binding.mapFragment.id, it).commit()
+            }
+        mapFragment.getMapAsync(this)
+
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions,
+                grantResults)) {
+            if (!locationSource.isActivated) { // 권한 거부됨
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            }
+            return
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onMapReady(map: NaverMap) {
+        naverMap = map
+        naverMap.locationSource = locationSource
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+        naverMap.uiSettings.isLocationButtonEnabled = true
+        naverMap.uiSettings.isZoomControlEnabled = false
+        naverMap.uiSettings.isIndoorLevelPickerEnabled = false
+        naverMap.isLiteModeEnabled = true
+//        naverMap.mapType = NaverMap.MapType.Navi
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 }
