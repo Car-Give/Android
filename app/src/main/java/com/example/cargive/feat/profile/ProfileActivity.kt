fe35468ae5459be5c5e99fcf668ae7008c96e34d
yeoroJeong <packageair@naver.com>
2023-08-02 16:25:26 +0900

    feat: 프로필 화면 구성

---
+++ app/src/main/java/com/example/cargive/feat/profile/ProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cargive.feat.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cargive.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
