26a8ac2af08cbd770c7f024c65c79ff434217b68
joheeho <whgmsla@gmail.com>
2023-08-07 12:23:15 +0900

    chore:gitignore style.xml 추가

--- app/src/main/java/com/example/cargive/feat/login/IntroActivity.kt
+++ app/src/main/java/com/example/cargive/feat/login/IntroActivity.kt
@@ -1,26 +1,35 @@
-package com.example.cargive.feat.login
+package com.example.registerlogin
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import androidx.appcompat.app.AppCompatActivity
-import androidx.viewbinding.ViewBinding
+import android.view.Window
+import android.view.WindowManager
 import com.example.cargive.databinding.ActivityIntroBinding
+import com.example.cargive.feat.login.LoginActivity
 
-class IntroActivity : AppCompatActivity() {
-    private lateinit var viewBinding:ActivityIntroBinding
+class IntroActivity : Activity() { // AppCompatActivity가 아닌 Activity를 상속받도록 변경
+
+    private lateinit var viewBinding: ActivityIntroBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewBinding = ActivityIntroBinding.inflate(layoutInflater)
-        setContentView(viewBinding.root) // 뷰 바인딩의 루트 뷰로 설정
 
-        // 2초 후에 로그인 화면으로 전환하기 위해 Handler를 사용하여 딜레이 설정
+        // 상단바 없애기
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        viewBinding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
         Handler().postDelayed({
-            // 로그인 화면으로 전환하는 인텐트 생성
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
-            finish() // 현재 인트로 화면 종료
-        }, 2000) // 2초(2000ms) 딜레이
+            finish()
+        }, 2000)
     }
 }
