731e64661e942b5f4cbe7c9af6ce3f659b9d160c
joheeho <whgmsla@gmail.com>
2023-08-16 18:34:04 +0900

    chore: sdk 종속성 추가

--- app/src/main/java/com/example/cargive/feat/login/LoginActivity.kt
+++ app/src/main/java/com/example/cargive/feat/login/LoginActivity.kt
@@ -4,14 +4,19 @@ import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.android.identity.Utility
 import com.example.cargive.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityLoginBinding
+    private var code = ""
+    private var accessToken = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityLoginBinding.inflate(layoutInflater)
+
         setContentView(viewBinding.root)
+
 
         viewBinding.kakaologinButton.setOnClickListener {
 
---
+++ app/src/main/java/com/example/cargive/feat/login/Myapplication.kt
@@ -0,0 +1,4 @@
+package com.example.cargive.feat.login
+
+class Myapplication {
+}
