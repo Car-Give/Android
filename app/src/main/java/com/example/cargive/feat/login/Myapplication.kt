9e23632b4f9acf02d08a672dacbaeac41ce9dc98
joheeho <whgmsla@gmail.com>
2023-08-16 18:58:23 +0900

    chore: sdk 초기화

--- app/src/main/java/com/example/cargive/feat/login/Myapplication.kt
+++ app/src/main/java/com/example/cargive/feat/login/Myapplication.kt
@@ -1,4 +1,14 @@
 package com.example.cargive.feat.login
 
-class Myapplication {
+import android.app.Application
+import com.example.cargive.BuildConfig
+import com.kakao.sdk.common.KakaoSdk
+
+class Myapplication : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        // 다른 초기화 코드들
+        // Kakao SDK 초기화
+        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
+    }
 }
